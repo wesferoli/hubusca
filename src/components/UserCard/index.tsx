e03910d45ca6a89048f2082f1a400dbf08e8ec7c
wesferoli <wesleyfer.oli97@gmail.com>
2021-12-22 23:03:14 -0300

    Componente do card do usuário

---
+++ src/components/UserCard/index.tsx
@@ -0,0 +1,186 @@
+import { useState } from "react";
+import { Image, Text, View } from "react-native"
+import Icon from "react-native-vector-icons/Entypo";
+import { UserDetail } from "../UserDetail";
+import { UserRepo } from "../UserRepo";
+import { 
+  Container,
+  LeftContainer,
+  RightContainer,
+  UserCardContainer
+} from "./styles";
+
+interface IUser {
+  login: string;
+  id: number;
+  node_id?: string;
+  avatar_url: string;
+  gravatar_id?: string;
+  url?: string;
+  html_url?: string;
+  followers_url?: string;
+  following_url?: string;
+  gists_url?: string;
+  starred_url?: string;
+  subscriptions_url?: string;
+  organizations_url?: string;
+  repos_url?: string;
+  events_url?: string;
+  received_events_url?: string;
+  type?: string;
+  site_admin?: boolean;
+  name: string;
+  company?: string;
+  blog?: string;
+  location: string;
+  email?: string;
+  hireable?: boolean;
+  bio?: string;
+  twitter_username?: string;
+  public_repos: number;
+  public_gists?: number;
+  followers: number;
+  following?: number;
+  created_at?: string;
+  updated_at?: string
+}
+
+interface IRepo {
+  id: number,
+  node_id?: string,
+  name: string,
+  full_name?: string,
+  private?: boolean,
+  owner?: {
+    login: string,
+    id: number,
+    node_id: string,
+    avatar_url: string,
+    gravatar_id: string,
+    url: string,
+    html_url: string,
+    followers_url: string,
+    following_url: string,
+    gists_url: string,
+    starred_url: string,
+    subscriptions_url: string,
+    organizations_url: string,
+    repos_url: string,
+    events_url: string,
+    received_events_url: string,
+    type: string,
+    site_admin: boolean
+  },
+  html_url: string,
+  description: string,
+  fork?: boolean,
+  url?: string,
+  forks_url?: string,
+  keys_url?: string,
+  collaborators_url?: string,
+  teams_url?: string,
+  hooks_url?: string,
+  issue_events_url?: string,
+  events_url?: string,
+  assignees_url?: string,
+  branches_url?: string,
+  tags_url?: string,
+  blobs_url?: string,
+  git_tags_url?: string,
+  git_refs_url?: string,
+  trees_url?: string,
+  statuses_url?: string,
+  languages_url?: string,
+  stargazers_url?: string,
+  contributors_url?: string,
+  subscribers_url?: string,
+  subscription_url?: string,
+  commits_url?: string,
+  git_commits_url?: string,
+  comments_url?: string,
+  issue_comment_url?: string,
+  contents_url?: string,
+  compare_url?: string,
+  merges_url?: string,
+  archive_url?: string,
+  downloads_url?: string,
+  issues_url?: string,
+  pulls_url?: string,
+  milestones_url?: string,
+  notifications_url?: string,
+  labels_url?: string,
+  releases_url?: string,
+  deployments_url?: string,
+  created_at: string,
+  updated_at?: string,
+  pushed_at: string,
+  git_url?: string,
+  ssh_url?: string,
+  clone_url?: string,
+  svn_url?: string,
+  homepage?: string,
+  size?: number,
+  stargazers_count?: number,
+  watchers_count?: number,
+  language: string,
+  has_issues?: boolean,
+  has_projects?: boolean,
+  has_downloads?: boolean,
+  has_wiki?: boolean,
+  has_pages?: boolean,
+  forks_count?: number,
+  mirror_url?: string,
+  archived?: boolean,
+  disabled?: boolean,
+  open_issues_count?: number,
+  license?: string,
+  allow_forking?: boolean,
+  is_template?: boolean,
+  topics?: [],
+  visibility?: string,
+  forks?: number,
+  open_issues?: number,
+  watchers?: number,
+  default_branch?: string
+}
+
+interface Props {
+  user: IUser
+  repos: IRepo[]
+}
+
+export const UserCard: React.FC<Props> = ({ user, repos }) => {
+  const [imageClick, setImageClick] = useState<Boolean>(false)
+
+  return (
+    <Container>
+      <UserCardContainer>
+        <LeftContainer onPress={() => imageClick ? setImageClick(false) : setImageClick(true)}>
+          <Image 
+            source={{uri: user.avatar_url}} 
+            style={{ width: 60, height: 60, borderRadius: 50 }}
+          />
+        </LeftContainer>
+
+        <RightContainer>
+          <Text style={{ fontWeight: 'bold', fontSize: 20 }}>{user.name}</Text>
+          <View style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+            <Icon name={"user"} size={12} color={'black'} />
+            <Text style={{ marginLeft: 5, fontStyle: "italic" }}>{user.login}</Text>
+          </View>
+          <View style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+            <Icon name={"location-pin"} size={12} color={'black'} />
+            <Text style={{ marginLeft: 5, fontStyle: "italic" }} >{user.location || 'N/A'}</Text>
+          </View>
+        </RightContainer>
+      </UserCardContainer>
+
+      { imageClick ? 
+        <>
+          <UserDetail user={user} />
+          <UserRepo repos={repos} />
+        </>
+       : null }
+    </Container>
+  )
+}
---
+++ src/components/UserCard/styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  width: 280px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  background-color: #F5F5F5;
+  padding: 10px;
+  
+  border-radius: 5px;
+`;
+
+export const UserCardContainer = styled.View`
+  display: flex;
+  flex-direction: row;
+
+  align-items: center;
+`;
+
+export const LeftContainer = styled.TouchableOpacity`
+`;
+
+export const RightContainer = styled.View`
+  display: flex;
+  margin-left: 15px;
+`;
