df8287bf3a245616a7bdc2106ff579da0cbddb50
wesferoli <wesleyfer.oli97@gmail.com>
2021-12-22 23:04:43 -0300

    Componente que lista os repositórios do usuário

---
+++ src/components/UserRepo/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  margin-top: 10px;
+  padding: 10px;
+  border-radius: 5px;
+
+  flex: 1;
+  justify-content: space-between;
+
+  background-color: #E5DADA;
+`
